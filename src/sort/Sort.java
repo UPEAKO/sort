45387f7fdc5c395969147a1fa3193c34f1041e1f
UPEAKO <u19680678@gmail.com>
2019-08-17 19:26:42 +0800

    add 2 alg

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -1,61 +1,52 @@
 package sort;
 /**
- * 所有排序算法均按升序
+ * All Ascending
  */
 
-public class Sort {
-    /**
-     * 交换元素
-     *
-     * @param array   数组
-     * @param target1 下标1
-     * @param target2 下标2
-     */
-    private void swap(int[] array, int target1, int target2) {
+public class Sort
+{
+
+    private static void swap(int[] array, int target1, int target2)
+    {
         int temp = array[target1];
         array[target1] = array[target2];
         array[target2] = temp;
     }
 
-    /**
-     * 打印显示数组
-     * @param array 数组
-     */
-    public void printArray(int[] array) {
-        for (int temp:
-                array) {
+    private static void printArray(int[] array)
+    {
+        for (int temp: array)
+        {
             System.out.print(temp + ", ");
         }
         System.out.print("\n");
     }
 
-    /**
-     * 冒泡排序
-     * @param array 待排序数组
-     */
-    public void bubbleSort(int[] array) {
+    private static void bubbleSort(int[] array)
+    {
         int length = array.length;
         if (length == 0)
             return;
-        for (int i = 0; i < length - 1; i++) {
-            for (int j = 0; j < length - 1 - i; j++) {
+        for (int i = 0; i < length - 1; i++)
+        {
+            for (int j = 0; j < length - 1 - i; j++)
+            {
                 if (array[j] > array[j + 1])
                     swap(array, j, j + 1);
             }
         }
     }
 
-    /**
-     * 选择排序
-     * @param array 待排序数组
-     */
-    public void selectSort(int[] array) {
+    private static void selectSort(int[] array)
+    {
         int length = array.length;
         if (length == 0)
             return;
-        for (int i = 0; i < length - 1; i++) {
+        for (int i = 0; i < length - 1; i++)
+        {
             int minIndex = i;
-            for (int j = i + 1; j < length; j++) {
+            for (int j = i + 1; j < length; j++)
+            {
                 if (array[j] < array[minIndex])
                     minIndex = j;
             }
@@ -63,28 +54,68 @@ public class Sort {
                 swap(array, i, minIndex);
         }
     }
-    /**
-     * 插入排序
-     * @param array 待排序数组
-     */
-    /*
-    public void insertSort(int[] array) {
+
+    private static void insertSort(int[] array)
+    {
         int length = array.length;
         if (length == 0)
             return;
-        for (int i = 1; i < length; i++) {
-
+        for (int i = 1; i < length; i++)
+        {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
         }
     }
 
-    public static void main(String[] args) {
-        //用于测试的数组
+    private static void quickSort(int[] array, int L, int R)
+    {
+        if (L < R)
+        {
+            int i = L, j = R, X = array[L];
+            while (i < j)
+            {
+                while (i < j && array[j] >= X)
+                    j--;
+                if (i < j)
+                    array[i++] = array[j];
+                while (i < j && array[i] < X)
+                    i++;
+                if (i < j)
+                    array[j--] = array[i];
+            }
+            array[i] = X;
+            quickSort(array, L, i - 1);
+            quickSort(array, i + 1, R);
+        }
+    }
+
+    public static void main(String[] args)
+    {
         int[] array = {4, 5, 1, 35, 12, 4, 6, 99, 123, 55};
-        Sort sort = new Sort();
-        sort.printArray(array);
-        //冒泡
-        //sort.bubbleSort(array);
-        sort.selectSort(array);
-        sort.printArray(array);
-    }*/
+
+        int[] workArray = array.clone();
+        printArray(workArray);
+        bubbleSort(workArray);
+        printArray(workArray);
+
+        workArray = array.clone();
+        printArray(workArray);
+        selectSort(workArray);
+        printArray(workArray);
+
+        workArray = array.clone();
+        printArray(workArray);
+        insertSort(workArray);
+        printArray(workArray);
+
+        workArray = array.clone();
+        printArray(workArray);
+        quickSort(workArray,0,workArray.length - 1);
+        printArray(workArray);
+    }
 }
